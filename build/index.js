2377dfc1207d7e3eef02a653ccfdaa758b0f6e96
Fabian Bentz <info@fabianbentz.de>
2022-06-10 16:46:25 +0200

    chore: inital commit

---
+++ build/index.js
@@ -0,0 +1,13 @@
+/*!
+ * 
+ *   stringles v0.0.0
+ *   https://github.com/bentzibentz/stringles
+ *
+ *   Copyright (c) Fabian Bentz (https://github.com/bentzibentz) and project contributors.
+ *
+ *   This source code is licensed under the MIT license found in the
+ *   LICENSE file in the root directory of this source tree.
+ *
+ */
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.stringles=t():e.stringles=t()}(self,(()=>(()=>{"use strict";var e={d:(t,o)=>{for(var r in o)e.o(o,r)&&!e.o(t,r)&&Object.defineProperty(t,r,{enumerable:!0,get:o[r]})},o:(e,t)=>Object.prototype.hasOwnProperty.call(e,t),r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},t={};function o(e,t){for(var o=0;o<t.length;o++){var r=t[o];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}function r(e,t,r){return t&&o(e.prototype,t),r&&o(e,r),Object.defineProperty(e,"prototype",{writable:!1}),e}e.r(t),e.d(t,{default:()=>n});const n=r((function e(){var t,o,r;!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,e),r=function(){return console.log("Library method fired"),!0},(o="myMethod")in(t=this)?Object.defineProperty(t,o,{value:r,enumerable:!0,configurable:!0,writable:!0}):t[o]=r,console.log("Library constructor loaded")}));return t})()));
+//# sourceMappingURL=index.js.map
